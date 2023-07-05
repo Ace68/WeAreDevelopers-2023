92df14fc32b0a33d7ccb0ef578240481f8fa9b39
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-07-05 16:24:30 +0200

    Complete example with Saga

--- ModularArchitecture/BrewUp.Modules.Purchases.Domain/CommandHandlers/ChangePurchaseOrderStatusToCompleteCommandHandler.cs
+++ ModularArchitecture/BrewUp.Modules.Purchases.Domain/CommandHandlers/ChangePurchaseOrderStatusToCompleteCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using BrewUp.Modules.Purchases.Messages.Commands;
 using BrewUp.Modules.Purchases.Messages.Events;
 using BrewUp.Shared.Abstracts;
+using BrewUp.Shared.DomainIds;
 using BrewUp.Shared.Dtos;
 using MediatR;
 
@@ -18,7 +19,26 @@ public class ChangePurchaseOrderStatusToCompleteCommandHandler : CommandHandlerB
 	public override async Task Handle(ChangePurchaseOrderStatusToComplete request, CancellationToken cancellationToken)
 	{
 		// Do something
-		var purchaseOrderStatusCompleted = new PurchaseOrderStatusChangedToComplete(request.PurchaseOrderId, Enumerable.Empty<OrderLine>());
+		var lines = Enumerable.Empty<OrderLine>();
+		lines = lines.Concat(new List<OrderLine>
+		{
+			new ()
+			{
+				BeerId = new BeerId(Guid.NewGuid()),
+				BeerName = new BeerName("Muflone IPA"),
+				Quantity = new Quantity()
+				{
+					Value = 10,
+					UnitOfMeasure = "NR"
+				},
+				Price = new Price()
+				{
+					Value = 2.5m,
+					Currency = "EUR"
+				},
+			},
+		});
+		var purchaseOrderStatusCompleted = new PurchaseOrderStatusChangedToComplete(request.PurchaseOrderId, lines);
 		await _serviceBus.Publish(purchaseOrderStatusCompleted, cancellationToken);
 	}
 }
